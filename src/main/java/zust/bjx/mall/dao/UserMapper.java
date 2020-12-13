cf831f4bfdb890dde35609a6b31efba2af826750
EnochStar <1181591005@zust.edu.cn>
2020-12-13 19:13:54 +0800

    商城模块之用户模块

---
+++ src/main/java/zust/bjx/mall/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package zust.bjx.mall.dao;
+
+import zust.bjx.mall.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
