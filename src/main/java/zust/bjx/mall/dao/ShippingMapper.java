5b02c0fafac07d8c7b036a035918d8fcb7e401b1
EnochStar <1181591005@zust.edu.cn>
2020-12-22 16:01:07 +0800

    商城模块之收获地址模块

---
+++ src/main/java/zust/bjx/mall/dao/ShippingMapper.java
@@ -0,0 +1,25 @@
+package zust.bjx.mall.dao;
+
+import org.apache.ibatis.annotations.Param;
+import zust.bjx.mall.pojo.Shipping;
+
+import java.util.List;
+
+public interface ShippingMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+
+    int deleteByIdAndUid(@Param("uid") Integer uid,
+                         @Param("shippingId") Integer shippingId);
+
+    List<Shipping> selectByUid(Integer uid);
+}
